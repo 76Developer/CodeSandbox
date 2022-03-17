53161febcc9a0d3d42ccb4898446a370fc648ef8
76Developer <101756080+76Developer@users.noreply.github.com>
2022-03-17 13:33:06 +0000

    Search component Refactor to Search component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import "./styles.css";
+import React from "react";
 
 const title = "React Starter";
 
 export default function App() {
-  const handleChange = (event) => {
-    console.log(event.target.value);
-  };
-
   return (
     <div className="App">
       <h1>{title}</h1>
+      <Search />
+    </div>
+  );
+}
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
       <div>
         <input type="text" id="search" onChange={handleChange} />
         <input type="button" id="searchButton" value="Search" />
       </div>
+      <div>Searching for: {searchTerm}</div>
     </div>
   );
-}
+};
