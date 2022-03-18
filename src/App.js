ba121b358cd78ffdcdd1806203774410fa602307
76Developer <101756080+76Developer@users.noreply.github.com>
2022-03-18 16:08:43 +0000

    Refactored Search

--- src/App.js
+++ src/App.js
@@ -4,28 +4,60 @@ import React from "react";
 const title = "React Starter";
 
 export default function App() {
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const searchTerms = [
+    {
+      name: "test",
+      id: 1
+    },
+    {
+      name: "word",
+      id: 2
+    },
+    {
+      name: "garden",
+      id: 3
+    }
+  ];
+
+  const handleSearch = (event) => {
+    console.log("handleSearch");
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedTerms = searchTerms.filter(function (search) {
+    console.log("Called SearchedTerms  " + searchTerm);
+    console.log(search.name.includes(searchTerm));
+    return search.name.includes(searchTerm);
+  });
+
   return (
     <div className="App">
       <h1>{title}</h1>
-      <Search />
+      <Search onSearch={handleSearch} />
+      <hr />
+      <List list={searchedTerms} />
     </div>
   );
 }
 
-const Search = () => {
-  const [searchTerm, setSearchTerm] = React.useState("");
+const List = (props) => {
+  return props.list.map((item) => (
+    <div key={item.id}>
+      <span>{item.name}</span>
+    </div>
+  ));
+};
 
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
+const Search = (props) => {
   return (
     <div>
       <div>
-        <input type="text" id="search" onChange={handleChange} />
+        <input type="text" id="search" onChange={props.onSearch} />
         <input type="button" id="searchButton" value="Search" />
       </div>
-      <div>Searching for: {searchTerm}</div>
+      <div>Searching for: </div>
     </div>
   );
 };
